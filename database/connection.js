66a6e959687fccdb7a42c402fcd63b4bad608929
Sancho Ibanez <maibanez1@up.edu.ph>
2023-03-22 13:09:04 +0800

    Update connection.js

--- database/connection.js
+++ database/connection.js
@@ -4,7 +4,14 @@ require('dotenv').config()
 const db = knex({
     client: 'pg',
 
-    connection: process.env.DATABASE
+    connection: {
+    	host : '127.0.0.1',
+    	port : 5432,
+    	user : 'postgres',
+    	password : '1234',
+    	database : 'postgres'
+	}
+
 })
 
 module.exports = db;
