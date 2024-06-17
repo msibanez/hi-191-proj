b62150fa9742cbf9ca4fa06721297fec81e85069
Sancho Ibanez <maibanez1@up.edu.ph>
2024-06-17 19:48:06 +0800

    changed connection

--- database/connection.js
+++ database/connection.js
@@ -4,13 +4,7 @@ require('dotenv').config()
 const db = knex({
     client: 'pg',
 
-    connection: {
-    	host : '127.0.0.1',
-    	port : 5432,
-    	user : 'postgres',
-    	password : '1234',
-			database : 'cancerdb'
-		}
+    connection: process.env.DATABASE
 
 })
 
